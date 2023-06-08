e00f7665957b2fa2a54157a1aef5567426889fdc
Swapnil Sharma <swapnil.sharma.cs.2019@miet.ac.in>
2023-06-08 20:52:42 +0530

    Update setup.py

--- setup.py
+++ setup.py
@@ -1,22 +1,21 @@
 from setuptools import find_packages, setup
-from typing import List
 
-with open("README.md", "n", encoding="utf-8") as f:
+with open("README.md", "r", encoding="utf-8") as f:
     long_description = f.read()
 
 __version__ = "0.0.0"
 
 REPO_NAME = "AI_ChatBot_For_Mental_Health"
 AUTHOR_USER_NAME = 'Swapnil-11-04-01'
-SRC_REPO = ''
+SRC_REPO = 'EVE_AI'
 AUTHOR_EMAIL = "swapnil.sharma.869.11@gmail.com"
 
 setup(
-    name=REPO_NAME,
+    name=SRC_REPO,
     version=__version__,
     author=AUTHOR_USER_NAME,
     author_email=AUTHOR_EMAIL,
-    description="AI Chatbot, named EVE AI, for thebetterment of the mental health.",
+    description="AI Chatbot, named EVE AI, for the betterment of the mental health.",
     long_description=long_description,
     long_descrption_content="text/markdown",
     url=(f'https://github.com/{AUTHOR_USER_NAME}/{REPO_NAME}'),
