08dc0ba685ffc9cda98a68e74163a15aed39bb2d
Swapnil Sharma <swapnil.sharma.cs.2019@miet.ac.in>
2023-06-11 17:07:01 +0530

    Create main.py

---
+++ main.py
@@ -0,0 +1,12 @@
+from EVE_AI import logger
+from EVE_AI.pipeline.stage_01_data_ingestion import DataIngestionTrainingPipeline
+
+STAGE_NAME = "Data Ingestion stage"
+try:
+   logger.info(f">>>>>> stage {STAGE_NAME} started <<<<<<")
+   data_ingestion = DataIngestionTrainingPipeline()
+   data_ingestion.main()
+   logger.info(f">>>>>> stage {STAGE_NAME} completed <<<<<<\n\nx==========x")
+except Exception as e:
+        logger.exception(e)
+        raise e
