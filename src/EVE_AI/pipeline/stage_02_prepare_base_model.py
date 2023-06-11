5f3b143723781dc7ab9449d84213174f6d2e1ee0
Swapnil Sharma <swapnil.sharma.cs.2019@miet.ac.in>
2023-06-12 00:34:29 +0530

    Update stage_02_prepare_base_model.py

---
+++ src/EVE_AI/pipeline/stage_02_prepare_base_model.py
@@ -0,0 +1,36 @@
+from EVE_AI.config.configuration import ConfigurationManager
+from EVE_AI.components.prepare_base_model import PrepareBaseModel
+from EVE_AI import logger
+
+STAGE_NAME = "Prepare base model"
+
+class PrepareBaseModelTrainingPipeline:
+    def __init__(self):
+        pass
+
+    def main(self):
+        config = ConfigurationManager()
+
+        prepare_base_model_config = config.get_prepare_base_model_config()
+        prepare_base_tokenizer_config = config.get_prepare_base_tokenizer_config()
+
+        prepare_base_model = PrepareBaseModel(config_model=prepare_base_model_config,
+                                              config_tokenizer=prepare_base_tokenizer_config)
+
+        prepare_base_model.get_base_model()
+        prepare_base_model.update_base_model()
+
+
+
+
+
+if __name__ == '__main__':
+    try:
+        logger.info(f"*******************")
+        logger.info(f">>>>>> stage {STAGE_NAME} started <<<<<<")
+        obj = PrepareBaseModelTrainingPipeline()
+        obj.main()
+        logger.info(f">>>>>> stage {STAGE_NAME} completed <<<<<<\n\nx==========x")
+    except Exception as e:
+        logger.exception(e)
+        raise e
