cd2bcd64d503563cbdfca5e3dcd236efd3aa08c2
Swapnil Sharma <swapnil.sharma.cs.2019@miet.ac.in>
2023-06-12 00:13:49 +0530

    creating base_model script for research

--- src/EVE_AI/config/configuration.py
+++ src/EVE_AI/config/configuration.py
@@ -1,6 +1,6 @@
 from EVE_AI.constants import *
 from EVE_AI.utils.common import read_yaml, create_directories
-from EVE_AI.entity.config_entity import DataIngestionConfig
+from EVE_AI.entity.config_entity import (DataIngestionConfig, PrepareBaseModelConfig, PrepareBaseTokenizerConfig)
 
 class ConfigurationManager:
     def __init__(
@@ -25,3 +25,32 @@ class ConfigurationManager:
         )
 
         return data_ingestion_config
+
+    def get_prepare_base_model_config(self) -> PrepareBaseModelConfig:
+        config_model = self.config.prepare_base_model
+
+        create_directories([config_model.root_dir])
+
+        prepare_base_model_config = PrepareBaseModelConfig(
+            root_dir=Path(config_model.root_dir),
+            base_model_path=Path(config_model.base_model_path),
+            updated_base_model_path=Path(config_model.updated_base_model_path),
+            params_learning_rate=self.params.LEARNING_RATE,
+            params_classes=self.params.CLASSES,
+            params_batch_size=self.params.BATCH_SIZE,
+            params_epochs=self.params.EPOCHS,
+        )
+
+        return prepare_base_model_config
+
+    def get_prepare_base_tokenizer_config(self) -> PrepareBaseTokenizerConfig:
+        config_tokenizer = self.config.prepare_base_tokenizer
+
+        create_directories([config_tokenizer.root_dir])
+
+        prepare_base_tokenizer_config = PrepareBaseTokenizerConfig(
+            root_dir=Path(config_tokenizer.root_dir),
+            base_tokenizer_path=Path(config_tokenizer.base_tokenizer_path)
+        )
+
+        return prepare_base_tokenizer_config
--- src/EVE_AI/entity/config_entity.py
+++ src/EVE_AI/entity/config_entity.py
@@ -8,3 +8,18 @@ class DataIngestionConfig:
     source_URL: str
     local_data_file: Path
     unzip_dir: Path
+
+@dataclass(frozen=True)
+class PrepareBaseModelConfig:
+    root_dir: Path
+    base_model_path: Path
+    updated_base_model_path: Path
+    params_learning_rate: float
+    params_classes: int
+    params_batch_size: int
+    params_epochs: int
+
+@dataclass(frozen=True)
+class PrepareBaseTokenizerConfig:
+    root_dir: Path
+    base_tokenizer_path: Path
