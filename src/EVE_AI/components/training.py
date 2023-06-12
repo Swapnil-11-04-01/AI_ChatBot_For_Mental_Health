af862d34d3ffd0d0e20d62d059fb6abf536caf8c
Swapnil Sharma <swapnil.sharma.cs.2019@miet.ac.in>
2023-06-12 18:17:15 +0530

    Create training.py

---
+++ src/EVE_AI/components/training.py
@@ -0,0 +1,53 @@
+import pickle
+from pathlib import Path
+import pandas as pd
+from EVE_AI.entity.config_entity import (TrainingConfig)
+import warnings
+
+warnings.filterwarnings("ignore")
+
+class Training:
+    def __init__(self, config: TrainingConfig):
+        self.config = config
+
+    def get_base_model(self):
+        with open(self.config.base_model_path, 'rb') as f:
+            self.model = pickle.load(f)
+
+    def get_tokenizer(self):
+        with open(self.config.base_tokenizer_path, 'rb') as f:
+            self.tokenizer = pickle.load(f)
+
+    def train_valid_generator(self):
+        self.training_data_frame = pd.read_csv(self.config.training_data)
+        self.train_text_generator = self.training_data_frame.iloc[:,0]
+        self.train_labels_generator = self.training_data_frame.iloc[:,1]
+
+        self.validation_data_frame = pd.read_csv(self.config.validation_data)
+        self.valid_text_generator = self.validation_data_frame.iloc[:,0]
+        self.valid_labels_generator = self.validation_data_frame.iloc[:,1]
+
+    @staticmethod
+    def save_model(path: Path, model):
+        with open(path, 'wb') as f:
+            pickle.dump(model, f)
+
+    def train(self):
+
+        self.tokenized_training_data = self.tokenizer.fit_transform(self.train_text_generator.tolist())
+        self.train_features = self.tokenized_training_data.toarray()
+
+        self.save_model(path=self.config.fitted_tokenizer_path, model=self.train_features)
+
+        self.tokenized_validation_data = self.tokenizer.transform(self.valid_text_generator.tolist())
+        self.valid_features = self.tokenized_validation_data.toarray()
+
+        self.train_labels = self.train_labels_generator
+        self.valid_labels = self.valid_labels_generator
+
+        self.model.fit(self.train_features,
+                       self.train_labels,
+                       eval_set=(self.valid_features, self.valid_labels),
+                       verbose=self.config.params_verbose)
+
+        self.save_model(path=self.config.trained_model_path, model=self.model)
