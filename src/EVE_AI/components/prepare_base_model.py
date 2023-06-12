75b8cfd1581d6dc37556273e9f339d1864a761a7
Swapnil Sharma <swapnil.sharma.cs.2019@miet.ac.in>
2023-06-12 17:57:01 +0530

    Update prepare_base_model.py

--- src/EVE_AI/components/prepare_base_model.py
+++ src/EVE_AI/components/prepare_base_model.py
@@ -1,71 +1,32 @@
 import pickle
 from pathlib import Path
-import tensorflow as tf
-from transformers import TFDistilBertForSequenceClassification, DistilBertTokenizer
 from EVE_AI.entity.config_entity import (PrepareBaseTokenizerConfig, PrepareBaseModelConfig)
+from sklearn.feature_extraction.text import TfidfVectorizer
+from catboost import CatBoostClassifier
+import warnings
+
+warnings.filterwarnings("ignore")
 
 class PrepareBaseModel:
     def __init__(self, config_model: PrepareBaseModelConfig, config_tokenizer: PrepareBaseTokenizerConfig):
         self.config_model = config_model
         self.config_tokenizer = config_tokenizer
 
-    def get_base_model(self):
-        self.model = TFDistilBertForSequenceClassification.from_pretrained(
-            'distilbert-base-uncased'
-        )
-
-        self.tokenizer = DistilBertTokenizer.from_pretrained(
-            'distilbert-base-uncased'
-        )
+    def get_ml_model(self):
+        self.model = CatBoostClassifier(iterations=self.config_model.params_iterations,
+                                        learning_rate=self.config_model.params_learning_rate,
+                                        depth=self.config_model.params_depth,
+                                        loss_function='MultiClass')
 
         self.save_model(path=self.config_model.base_model_path, model=self.model)
+
+
+    def get_tokenizer(self):
+        self.tokenizer = TfidfVectorizer()
         self.save_model(path=self.config_tokenizer.base_tokenizer_path, model=self.tokenizer)
 
-    @staticmethod
-    def _prepare_full_model(model, classes, freeze_all, freeze_till, learning_rate):
-        if freeze_all:
-            for layer in model.layers:
-                model.trainable = False
-        elif (freeze_till is not None) and (freeze_till > 0):
-            for layer in model.layers[:-freeze_till]:
-                model.trainable = False
-
-        model.summary()
-
-        # Define the sentiment analysis model based on DistilBERT
-        input_ids = tf.keras.Input(shape=(None,), dtype=tf.int32, name='input_ids')
-        attention_mask = tf.keras.Input(shape=(None,), dtype=tf.int32, name='attention_mask')
-
-        distilbert_output = model.distilbert([input_ids, attention_mask])
-
-        full_model = tf.keras.Model(inputs=[input_ids, attention_mask], outputs=distilbert_output)
-
-        # Modify the output layer for 6 emotions
-        full_model.layers[-1].activation = tf.keras.activations.softmax
-        full_model.layers[-1].units = classes
-        full_model.layers[-1].kernel_initializer = tf.keras.initializers.TruncatedNormal(stddev=0.02)
-
-        # Compile the model
-        optimizer = tf.keras.optimizers.Adam(learning_rate=learning_rate)
-        loss = tf.keras.losses.SparseCategoricalCrossentropy(from_logits=True)
-        metrics = ['accuracy']
-        full_model.compile(optimizer=optimizer, loss=loss, metrics=metrics)
-
-        full_model.summary()
-        return full_model
-
-    def update_base_model(self):
-        self.full_model = self._prepare_full_model(
-            model=self.model,
-            classes=self.config_model.params_classes,
-            freeze_all=True,
-            freeze_till=None,
-            learning_rate=self.config_model.params_learning_rate
-        )
-
-        self.save_model(path=self.config_model.updated_base_model_path, model=self.full_model)
 
     @staticmethod
-    def save_model(path: Path, model: tf.keras.Model):
+    def save_model(path: Path, model):
         with open(path, 'wb') as f:
             pickle.dump(model, f)
