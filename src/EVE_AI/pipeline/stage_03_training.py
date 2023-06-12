dcbc2786fb8edb75823ea256259d2c7a0e70cab9
Swapnil Sharma <swapnil.sharma.cs.2019@miet.ac.in>
2023-06-12 18:17:26 +0530

    Create stage_03_training.py

---
+++ src/EVE_AI/pipeline/stage_03_training.py
@@ -0,0 +1,32 @@
+from EVE_AI.config.configuration import ConfigurationManager
+from EVE_AI.components.training import Training
+from EVE_AI import logger
+
+STAGE_NAME = "Training"
+
+class ModelTrainingPipeline:
+    def __init__(self):
+        pass
+
+    def main(self):
+        config = ConfigurationManager()
+        training_config = config.get_training_config()
+        training = Training(config=training_config)
+        training.get_base_model()
+        training.get_tokenizer()
+        training.train_valid_generator()
+        training.train()
+
+
+
+
+if __name__ == '__main__':
+    try:
+        logger.info(f"*******************")
+        logger.info(f">>>>>> stage {STAGE_NAME} started <<<<<<")
+        obj = ModelTrainingPipeline()
+        obj.main()
+        logger.info(f">>>>>> stage {STAGE_NAME} completed <<<<<<\n\nx==========x")
+    except Exception as e:
+        logger.exception(e)
+        raise e
