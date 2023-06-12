fed378191f7ba73393c607935bb487fa0ad01904
Swapnil Sharma <swapnil.sharma.cs.2019@miet.ac.in>
2023-06-13 04:10:26 +0530

    Update main.py

--- main.py
+++ main.py
@@ -1,6 +1,9 @@
 from EVE_AI import logger
 from EVE_AI.pipeline.stage_01_data_ingestion import DataIngestionTrainingPipeline
 from EVE_AI.pipeline.stage_02_prepare_base_model import PrepareBaseModelTrainingPipeline
+from EVE_AI.pipeline.stage_03_training import ModelTrainingPipeline
+from EVE_AI.pipeline.stage_04_evaluation import EvaluationPipeline
+
 
 STAGE_NAME = "Data Ingestion stage"
 try:
@@ -12,6 +15,9 @@ except Exception as e:
         logger.exception(e)
         raise e
 
+
+
+
 STAGE_NAME = "Prepare base model"
 try:
    logger.info(f"*******************")
@@ -19,6 +25,37 @@ try:
    prepare_base_model = PrepareBaseModelTrainingPipeline()
    prepare_base_model.main()
    logger.info(f">>>>>> stage {STAGE_NAME} completed <<<<<<\n\nx==========x")
+except Exception as e:
+        logger.exception(e)
+        raise e
+
+
+
+
+STAGE_NAME = "Training"
+try:
+   logger.info(f"*******************")
+   logger.info(f">>>>>> stage {STAGE_NAME} started <<<<<<")
+   model_trainer = ModelTrainingPipeline()
+   model_trainer.main()
+   logger.info(f">>>>>> stage {STAGE_NAME} completed <<<<<<\n\nx==========x")
+except Exception as e:
+        logger.exception(e)
+        raise e
+
+
+
+
+
+
+STAGE_NAME = "Evaluation stage"
+try:
+   logger.info(f"*******************")
+   logger.info(f">>>>>> stage {STAGE_NAME} started <<<<<<")
+   model_evalution = EvaluationPipeline()
+   model_evalution.main()
+   logger.info(f">>>>>> stage {STAGE_NAME} completed <<<<<<\n\nx==========x")
+
 except Exception as e:
         logger.exception(e)
         raise e
