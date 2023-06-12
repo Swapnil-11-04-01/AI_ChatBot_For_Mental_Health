246dbc9f67d9290334170e15ab6be928ab7f6aaa
Swapnil Sharma <swapnil.sharma.cs.2019@miet.ac.in>
2023-06-13 04:11:56 +0530

    Create stage_04_evaluation.py

---
+++ src/EVE_AI/pipeline/stage_04_evaluation.py
@@ -0,0 +1,33 @@
+from EVE_AI.config.configuration import ConfigurationManager
+from EVE_AI.components.evaluation import Evaluation
+from EVE_AI import logger
+import warnings
+
+warnings.filterwarnings("ignore")
+
+STAGE_NAME = "Evaluation stage"
+
+
+class EvaluationPipeline:
+    def __init__(self):
+        pass
+
+    def main(self):
+        config = ConfigurationManager()
+        val_config = config.get_validation_config()
+        evaluation = Evaluation(val_config)
+        evaluation._valid_generator()
+        evaluation.evaluation()
+        evaluation.save_score()
+
+
+if __name__ == '__main__':
+    try:
+        logger.info(f"*******************")
+        logger.info(f">>>>>> stage {STAGE_NAME} started <<<<<<")
+        obj = EvaluationPipeline()
+        obj.main()
+        logger.info(f">>>>>> stage {STAGE_NAME} completed <<<<<<\n\nx==========x")
+    except Exception as e:
+        logger.exception(e)
+        raise e
