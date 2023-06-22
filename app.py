0af0a0c320bdc5b8e98b4e0f1a207f844de6a3d7
Swapnil Sharma <swapnil.sharma.cs.2019@miet.ac.in>
2023-06-23 02:18:23 +0530

    Update app.py

--- app.py
+++ app.py
@@ -3,20 +3,3 @@ from streamlit_chat import message
 
 message("My message")
 message("Hello bot!", is_user=True)  # align's the message to the right
-
-import pyttsx3
-
-def speak(text):
-    engine = pyttsx3.init()
-    engine.setProperty("rate", 150)  # Adjust the speech rate (optional)
-    engine.say(text)
-    engine.runAndWait()
-
-def main():
-    st.title("Text-to-Speech Demo")
-    text_input = st.text_input("Enter the text to be spoken")
-    if st.button("Speak"):
-        speak(text_input)
-
-if __name__ == "__main__":
-    main()
