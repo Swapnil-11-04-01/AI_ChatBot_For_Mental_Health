d6d58dadb1b9cb91ce159c06ceb77b817e7c4736
Swapnil Sharma <swapnil.sharma.cs.2019@miet.ac.in>
2023-06-23 15:27:52 +0530

    Update app.py

--- app.py
+++ app.py
@@ -1,55 +1,44 @@
-from EVE_AI.components.base import Base
-from EVE_AI.config.configuration import ConfigurationManager
 import streamlit as st
 from streamlit_chat import message
 import pygame
 
+def typewriter_animation(text, delay=100):
+    """
+    Displays text one character at a time, with a delay between each character.
 
-sound = True
-pygame.mixer.init()
-user_history = []
+    Args:
+        text (str): The text to display.
+        delay (int): The delay between each character, in milliseconds.
+    """
+    for i, char in enumerate(text):
+        st.write(char)
+        pygame.time.wait(delay)
 
-if "history" not in st.session_state:
-    st.session_state.history = []
+def generate_answer(msg):
+    user_message = st.session_state.input_text
+    message_bot = msg
+    # eve.speak(message_bot)
 
-st.title("Hello Chatbot")
-
-config = ConfigurationManager()
-base_config = config.get_base_config()
-eve = Base(config=base_config)
-
-
-ques_set_0 = eve.questions_0
-
-if "input_text" not in st.session_state:
-    st.session_state.input_text = ""
-
-def generate_answer(key, value):
-    if key == "name":
-        user_message = st.session_state.input_text
-        eve.user_name = user_message
-        message_bot = value
-
-    else:
-        user_message = st.session_state.input_text
-        message_bot = value
+    if "history" not in st.session_state:
+        st.session_state.history = []
 
     st.session_state.history.append({"message": user_message, "is_user": True})
+    typewriter_animation(message_bot)
     st.session_state.history.append({"message": message_bot, "is_user": False})
 
-for i, chat in enumerate(st.session_state.history):
-    message(**chat, key=str(i)) #unpacking
-
-c = 0
-for key, value in ques_set_0.items():
-    key = f"input_text_{c}"
-    c+=1
-    if key == "intro":
-        message(value, key="intro")
+def reply(state=None):
+    if state==1:
+        pass
     else:
-        msg = st.text_input("Talk to the bot", key=key, on_change=generate_answer(key, value))
-        user_history.append(msg)
+        msg = "hi"
+        generate_answer(msg)
 
+input_text = st.text_input("Talk to the bot", key="input_text", on_change=reply)
+button = st.button("Submit")
+
+if button:
+    reply(state=1)
+
+message("Hello")
 for i, chat in enumerate(st.session_state.history):
     message(**chat, key=str(i)) #unpacking
-
